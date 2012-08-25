b36ac9cf3a1c51afa6b25129aab7990242eb8d29
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-25 09:54:34 -0430

    llamando a los eventos

--- Kernel.php
+++ Kernel.php
@@ -5,6 +5,11 @@ namespace KumbiaPHP\Kernel;
 use KumbiaPHP\Kernel\KernelInterface;
 //use KumbiaPHP\Kernel\Session\Session;
 use KumbiaPHP\Kernel\Controller\ControllerResolver;
+use KumbiaPHP\Kernel\Event\KumbiaEvents;
+use KumbiaPHP\EventDispatcher\EventDispatcher;
+use KumbiaPHP\Kernel\Event\RequestEvent;
+use KumbiaPHP\Kernel\Event\ControllerEvent;
+use KumbiaPHP\Kernel\Event\ResponseEvent;
 
 /**
  * Description of Kernel
@@ -24,15 +29,37 @@ abstract class Kernel implements KernelInterface
     //put your code here
     public function execute(Request $request)
     {
+        $dispatcher = new EventDispatcher();
+
+        //ejecutamos el evento request
+        $dispatcher->dispatch(KumbiaEvents::REQUEST, new RequestEvent($request));
+
         $resolver = new ControllerResolver($this->getModules(), $this->getDefaultModule());
 
         list($controller, $action, $params) = $resolver->getController($request);
 
+        //ejecutamos el evento controller.
+        $dispatcher->dispatch(KumbiaEvents::CONTROLLER, new ControllerEvent($request, array($controller, $action)));
+
         $response = call_user_func_array(array($controller, $action), $params);
 
+
         if (!$response instanceof Response) {
-            throw new \LogicException("La accion $action del controlador " . get_class($controller) . " Debe retornar una instancia de Response");
+
+            $response = $controller->getResponse();
+            //si la acción no devolvió respuesta,
+            //la buscamos en el controlador
+            if (!$response instanceof Response) {
+                //si en ningun momento se creó una Respuesta, la creamos
+
+                $response = new Response();
+            }
+            //throw new \LogicException("La accion $action del controlador " . get_class($controller) . " Debe retornar una instancia de Response");
         }
+
+        //ejecutamos el evento response.
+        $dispatcher->dispatch(KumbiaEvents::RESPONSE, new ResponseEvent($request, $response));
+
         return $response;
     }
 
