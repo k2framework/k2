6490b76dc682a423242af6aeebe262757bca6ba9
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-29 00:52:37 -0430

    nuevo metodo link

--- core/KumbiaPHP/View/Helper/Html.php
+++ core/KumbiaPHP/View/Helper/Html.php
@@ -18,4 +18,20 @@ class Html extends AbstractHelper
         return "<img src=\"$src\" alt=\"$alt\" />";
     }
 
+    /**
+     * Crea un enlace a otra direccion en la app
+     * 
+     * @param string $action Ruta a la acción
+     * @param string $text Texto a mostrar
+     * @param string|array $attrs Atributos adicionales
+     * @return string
+     */
+    public function link($action, $text, $attrs = NULL)
+    {
+//        if (is_array($attrs)) {
+//            $attrs = Tag::getAttrs($attrs);
+//        }
+        return '<a href="' . $this->app->getBaseUrl() . "$action\" $attrs >$text</a>";
+    }
+
 }
