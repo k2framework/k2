0b6a8e56f4869a8fd9aea6b8d0d038e72233d413
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-29 00:53:04 -0430

    limpiando el controlador

--- default/app/modules/Index/Controller/IndexController.php
+++ default/app/modules/Index/Controller/IndexController.php
@@ -11,15 +11,10 @@ use KumbiaPHP\Kernel\Controller\Controller;
  */
 class IndexController extends Controller
 {
-
-    public function index(\KumbiaPHP\Kernel\Request $request,$param = NULL)
-    {                
-        //return new \KumbiaPHP\Kernel\Response($request->getRequestUri() . "  <br/> $param");
-    }
-
-    public function accion2()
-    {
-        $this->get('otro_servicio')->mensaje($this->get("flash")->get("error"));
+    
+    public function index()
+    {           
+        var_dump($this->getRequest()->server);
     }
 
 }
