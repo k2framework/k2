3a0e23f9b654a3f29bffade539a4cd70b79682ab
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-30 09:02:59 -0430

    creando metodo estatico getContainer
    El cual Devuelve el objeto container para casos especiales donde
    no sea posible pasarlo a travez del inyector de dependencias,
    por ejemplo en el active record.

--- core/KumbiaPHP/Kernel/KernelInterface.php
+++ core/KumbiaPHP/Kernel/KernelInterface.php
@@ -18,4 +18,15 @@ interface KernelInterface
      * @return Response 
      */
     public function execute(Request $request);
+    
+    /**
+     * Devuelve el objeto container para casos especiales donde
+     * no sea posible pasarlo a travez del inyector de dependencias,
+     * por ejemplo en el active record.
+     * 
+     * Por favor evitar en los posible su uso
+     * 
+     * @return ContainerInterface
+     */
+    public static function getContainer();
 }
