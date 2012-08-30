2a7dea5af271b0254bbf2a249887210f5c690dcd
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-30 12:00:47 -0430

    agregada propiedad estatica con los textos de cada status de respuesta

--- core/KumbiaPHP/Kernel/Response.php
+++ core/KumbiaPHP/Kernel/Response.php
@@ -34,19 +34,56 @@ class Response
      *
      * @var type 
      */
-    protected $statusText;
-
-    /**
-     *
-     * @var type 
-     */
     protected $charset;
+    public static $statusTexts = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+    );
 
     public function __construct($content = NULL, $statusCode = 200, array $headers = array())
     {
         $this->content = $content;
         $this->statusCode = $statusCode;
-        $this->statusText = "Ok";
+        $this->statusText = isset(self::$statusTexts[$statusCode]) ? self::$statusTexts[$statusCode] : NULL;
         $this->headers = new Parameters($headers);
     }
 
@@ -106,7 +143,7 @@ class Response
             $charset = $this->getCharset() ? : 'UTF-8';
             $this->headers->set('Content-Type', "text/html; charset=$charset");
         }
-        
+
         //mandamos el status
         header(sprintf('HTTP/1.0 %s %s', $this->statusCode, $this->statusText));
 
