d1efebc61a543e7a202ed50db249d2e77308e24e
Manuel Aguirre <programador.manuel@gmail.com>
2012-08-30 20:28:49 -0430

    nuevo modelo para probar el active record

---
+++ default/app/modules/Index/Model/Usuarios.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Index\Model;
+
+use KumbiaPHP\KumbiaActiveRecord\ActiveRecord;
+
+/**
+ * Description of Usuarios
+ *
+ * @author maguirre
+ */
+class Usuarios extends ActiveRecord
+{
+    //put your code here
+}
