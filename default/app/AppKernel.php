cb8bb9a5b0c40f38e7907776852a0d2ebf8ba4e5
Manuel Aguirre <programador.manuel@gmail.com>
2012-09-01 00:35:26 -0430

    nueva forma de registrar los modulos.
    Ahora se registran en el metodo registerRoutes
    
    donde se devuelve un arreglo en el que los indices son prefijos de rutas
    que si coinciden con el inicio de la url ejecutan el modulo asociado
    como valor de dicho indice.
    
    Siempre deben colocarse las rutas de las mas especificas a las más
    generales, ya que el router se quedará con la primera coincidencia de
    los prefijos.

--- default/app/AppKernel.php
+++ default/app/AppKernel.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once '../../core/KumbiaPHP/Kernel/Kernel.php';
 
 use KumbiaPHP\Kernel\Kernel;
@@ -15,9 +14,18 @@ class AppKernel extends Kernel
     protected function registerNamespaces()
     {
         return array(
+            'modules' => __DIR__ . '/modules/',
             'KumbiaPHP' => __DIR__ . '/../../core/',
-            'Index' => __DIR__ . '/modules/',
-            'Rest' => __DIR__ . '/modules/Demos/',
+            'ActiveRecord' => __DIR__ . '/../../core/',
+        );
+    }
+
+    protected function registerRoutes()
+    {
+        return array(
+            '/demo/rest' => __DIR__ . '/modules/Demos/Rest/',
+            '/demo/router' => __DIR__ . '/modules/Demos/Router/',
+            '/' => __DIR__ . '/modules/Index/',
         );
     }
 
