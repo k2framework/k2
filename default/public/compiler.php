6ece8731f21eec4a31ec66941288cf5d09d2748e
Manuel Aguirre <programador.manuel@gmail.com>
2012-10-22 21:38:15 -0430

    agregado un compiler, el cual crea el archivo app/kernel.min.php

---
+++ default/public/compiler.php
@@ -0,0 +1,23 @@
+<?php
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1'))) {
+    header('HTTP/1.0 403 Forbidden');
+    exit();
+}
+
+require_once '../app/AppKernel.php';
+
+use KumbiaPHP\Kernel\Request;
+use KumbiaPHP\Compiler\Compiler;
+
+$compiler = new Compiler(__DIR__ . '/../app/kernel.min.php', TRUE);
+$compiler->registerDirectories(array(
+    'KumbiaPHP' => __DIR__ . '/../../vendor/kumbiaphp/kumbiaphp/src/',
+));
+
+$app = new AppKernel(false);
+
+$app->execute(new Request());
+
+$compiler->compile();
+
+die("Se ha compilado el core del Framework");
