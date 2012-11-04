a21a4583546c70b22e6e019df4aa588304fd06fe
Manuel Aguirre <programador.manuel@gmail.com>
2012-11-03 19:42:06 -0430

    cargando el kernel.min.php

--- default/public/index.php
+++ default/public/index.php
@@ -2,7 +2,7 @@
 
 define('START_TIME', microtime(1));
 
-//require_once '../app/kernel.min.php';
+require_once '../app/kernel.min.php';
 require_once '../app/AppKernel.php';
 
 use KumbiaPHP\Kernel\Request;
