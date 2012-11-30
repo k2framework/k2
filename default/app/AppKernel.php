4066b0df0b4e4f0bdf494fa66679369d9435c28c
root <root@dhcppc0.unassigned>
2012-11-30 23:16:26 +0000

    Obteniendo la instancia del loader en el AppKernel...\n Esto permitirá agregar rutas de librerias

--- default/app/AppKernel.php
+++ default/app/AppKernel.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+
+/* @var $loader Composer\Autoload\ClassLoader */
+$loader = require_once __DIR__ . '/../../vendor/autoload.php';
 
 use KumbiaPHP\Kernel\Kernel;
 
@@ -8,39 +10,39 @@ use KumbiaPHP\Kernel\Kernel;
  *
  * @author manuel
  */
-class AppKernel extends Kernel
-{
+class AppKernel extends Kernel {
 
-    protected function registerModules()
-    {
+    protected function registerModules() {
         $modules = array(
-            'KumbiaPHP'   => __DIR__ . '/../../vendor/kumbiaphp/core/src/',
-            'Index'       => __DIR__ . '/modules/',
+            'KumbiaPHP' => __DIR__ . '/../../vendor/kumbiaphp/core/src/',
+            'Index' => __DIR__ . '/modules/',
         );
 
         if (!$this->production) {
-            $modules['Demos/Rest']              = __DIR__ . '/modules/';
-            $modules['Demos/Router']            = __DIR__ . '/modules/';
-            $modules['Demos/Vistas']            = __DIR__ . '/modules/';
-            $modules['Demos/Modelos']           = __DIR__ . '/modules/';
-            $modules['Demos/SubiendoArchivos']  = __DIR__ . '/modules/';
-            $modules['Demos/Seguridad']         = __DIR__ . '/modules/';
+            $modules['Demos/Rest'] = __DIR__ . '/modules/';
+            $modules['Demos/Router'] = __DIR__ . '/modules/';
+            $modules['Demos/Vistas'] = __DIR__ . '/modules/';
+            $modules['Demos/Modelos'] = __DIR__ . '/modules/';
+            $modules['Demos/SubiendoArchivos'] = __DIR__ . '/modules/';
+            $modules['Demos/Seguridad'] = __DIR__ . '/modules/';
         }
 
         return $modules;
     }
 
-    protected function registerRoutes()
-    {
+    protected function registerRoutes() {
         return array(
-            '/'                 => 'Index',
-            '/demo/rest'        => 'Demos/Rest',
-            '/demo/router'      => 'Demos/Router',
-            '/demo/vistas'      => 'Demos/Vistas',
-            '/demo/modelos'     => 'Demos/Modelos',
-            '/demo/upload'      => 'Demos/SubiendoArchivos',
-            '/admin'            => 'Demos/Seguridad',
+            '/' => 'Index',
+            '/demo/rest' => 'Demos/Rest',
+            '/demo/router' => 'Demos/Router',
+            '/demo/vistas' => 'Demos/Vistas',
+            '/demo/modelos' => 'Demos/Modelos',
+            '/demo/upload' => 'Demos/SubiendoArchivos',
+            '/admin' => 'Demos/Seguridad',
         );
     }
 
 }
+
+//acá podemos incluir rutas y prefijos al autoloader
+//$loader->add('PHPExcel', __DIR__ . '../../vendor/');
