21ec707f7356f81e2247fec8ec1758dc8d5abd3f
manuel_j555 <programador.manuel@gmail.com>
2013-05-19 23:00:12 -0430

    mejorando el manejo de las excepciones

--- default/app/bootstrap.php
+++ default/app/bootstrap.php
@@ -4,12 +4,33 @@ use K2\Kernel\App;
 use K2\Kernel\Exception\ExceptionHandler;
 
 $loader = require_once __DIR__ . '/../../vendor/autoload.php';
-
+/*
+ */
+###### Especificamos si el proyecto está en producción o no ############
+define('PRODUCTION', false);
+ExceptionHandler::handle(false);
+/*
+ */
 define('APP_PATH', __DIR__);
+define('START_TIME', microtime(1));
+
+###### obtenemos la url de la petición ############
+$_url = isset($_GET['_url']) ? $_GET['_url'] : '/';
+
+###### Creamos el PUBLIC_PATH ############
+if ($_SERVER['QUERY_STRING']) {
+    $uri = $_SERVER['REQUEST_URI'];
+    if (false !== ($pos = strpos($_SERVER['REQUEST_URI'], '?'))) {
+        $uri = substr($_SERVER['REQUEST_URI'], 0, $pos);
+    }
+    define('PUBLIC_PATH', str_replace($_url, '/', urldecode($uri)));
+    unset($uri);
+} else {
+    define('PUBLIC_PATH', $_SERVER['REQUEST_URI']);
+}
 
 App::setLoader($loader);
 
-ExceptionHandler::handle();
 
 if (PRODUCTION) {
     error_reporting(0);
--- default/public/index.php
+++ default/public/index.php
@@ -3,28 +3,8 @@
 use K2\Kernel\Kernel;
 use K2\Kernel\Request;
 
-define('START_TIME', microtime(1));
-
-###### Especificamos si el proyecto está en producción o no ############
-define('PRODUCTION', false);
-
-###### obtenemos la url de la petición ############
-$_url = isset($_GET['_url']) ? $_GET['_url'] : '/';
-
-###### Creamos el PUBLIC_PATH ############
-if ($_SERVER['QUERY_STRING']) {
-    $uri = $_SERVER['REQUEST_URI'];
-    if (false !== ($pos = strpos($_SERVER['REQUEST_URI'], '?'))) {
-        $uri = substr($_SERVER['REQUEST_URI'], 0, $pos);
-    }
-    define('PUBLIC_PATH', str_replace($_url, '/', urldecode($uri)));
-    unset($uri);
-} else {
-    define('PUBLIC_PATH', $_SERVER['REQUEST_URI']);
-}
-
-###### Arrancamos la config inical del Framework ############
 //require_once '../app/kernel.min.php'; //usar las clases compiladas (mejora velocidad)
+###### Arrancamos la config inical del Framework ############
 require_once '../app/bootstrap.php';
 
 ###### Arrancamos y ejecutamos el FW ############
